056ef99db602458395889937fef26182c03b68ec
RodrigoA15 <rapapamija@misena.edu.co>
2025-08-06 14:08:04 -0500

    Fix: routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,12 +4,6 @@ import hidrucarburosRoutes from "./hidrucarburos.routes.js";
 const router = Router();
 const pathRoutes = "/api/v1";
 
-router.use(`${pathRoutes}`, (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "Welcome to the Hidrocarburos API",
-  });
-});
 router.use(`${pathRoutes}/hidrocarburos`, hidrucarburosRoutes);
 
 export default router;
