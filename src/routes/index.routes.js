d5927dc9b0819c6fa725e57f050ef4a22b922ae1
RodrigoA15 <rapapamija@misena.edu.co>
2025-08-06 13:12:33 -0500

    Fix: route welcome

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,7 +4,7 @@ import hidrucarburosRoutes from "./hidrucarburos.routes.js";
 const router = Router();
 const pathRoutes = "/api/v1";
 
-router.get("/", (req, res) => {
+router.use(`${pathRoutes}`, (req, res) => {
   res.status(200).json({
     status: "success",
     message: "Welcome to the Hidrocarburos API",
