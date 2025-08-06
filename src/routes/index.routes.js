92af0a0c5b6b4742b45e5cdb142419840a3e8bf2
RodrigoA15 <rapapamija@misena.edu.co>
2025-08-06 13:11:03 -0500

    add: welcome

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,6 +4,12 @@ import hidrucarburosRoutes from "./hidrucarburos.routes.js";
 const router = Router();
 const pathRoutes = "/api/v1";
 
+router.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Welcome to the Hidrocarburos API",
+  });
+});
 router.use(`${pathRoutes}/hidrocarburos`, hidrucarburosRoutes);
 
 export default router;
