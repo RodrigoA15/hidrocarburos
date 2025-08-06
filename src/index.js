9d2911be3d6e8babcf44b3f9b42394ed9b9d2c6f
RodrigoA15 <rapapamija@misena.edu.co>
2025-08-06 12:58:11 -0500

    Fix: Configuration veercel file

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ const PORT = 4500;
 async function main() {
   try {
     console.log("Database initialized");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
   } catch (error) {
     if (error instanceof Error) {
       console.log(error.message);
